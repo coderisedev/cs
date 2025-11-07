3c2f8caef801ddec7b0afcbc83250428357818e4
coderisedev <coderisedev@example.com>
2025-11-07 15:00:57 +0000

    chore(strapi): configure r2 upload provider

--- apps/strapi/config/plugins.ts
+++ apps/strapi/config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi/provider-upload-aws-s3",
+      providerOptions: {
+        baseUrl: env("AWS_PUBLIC_URL", env("AWS_ENDPOINT")),
+        rootPath: env("AWS_S3_PATH_PREFIX", ""),
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          endpoint: env("AWS_ENDPOINT"),
+          region: env("AWS_REGION", "auto"),
+          forcePathStyle: env.bool("AWS_FORCE_PATH_STYLE", false),
+          signatureVersion: "v4",
+          params: {
+            Bucket: env("AWS_BUCKET_NAME"),
+            ACL: env("AWS_ACL", "public-read"),
+          },
+        },
+      },
+    },
+  },
+})
